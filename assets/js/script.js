0efc2ba6cb0760fd19bf988306e1cfc62e47fd95
camyacodes <camyacodes@gmail.com>
2021-11-21 11:01:22 -0500

    added save and load functions for events

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,8 +2,20 @@ $(document).ready(function(){
     // Display current date using moment.js
     var currentDay = moment().format("MMMM Do, YYYY");
     $("#currentDay").text(currentDay)
-    // event listener for save button
+    // save event function
+    var saveEvent = function(){
+        var eventDescript = $("#event").val()
+        localStorage.setItem("event", JSON.stringify(eventDescript))
+    }
+    // load event function
+    var loadEvent = function(){
+        var savedEvents = JSON.parse(localStorage.getItem("event"));
+        $("#event").val(savedEvents)
+    }
+    // when save button is clicked, save the event in local storage
     $("#button").click(function(){
-        console.log("Hello World")
+        saveEvent();
     })
+    // load events saved in local storage
+    loadEvent();
 })
